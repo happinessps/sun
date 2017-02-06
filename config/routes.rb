0c527192c7e3cc41cb56d299c07430192708ede4
Rails Udemy course <happinessps@naver.com>
2017-02-06 17:37:53 +0000

    Fix create, add flash and error massages, create show

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-root 'welcome#home'
-get 'about', to: 'welcome#about'
+root 'page#home'
+get 'about', to: 'page#about'
 resources :articles
 
   # Example of regular route:
